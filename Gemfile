245adcb9d17af0e07712eb85dc16b48fc387b996
Edward Andress <eddie_andress@hotmail.com>
2016-07-07 14:40:58 +0100

    add factory girl to test group

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 group :test do
+  gem 'factory_girl_rails'
   gem 'faker'
 end
 
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,10 @@ module CorrespondenceToolPublic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.factory_girl false
+    end
+
   end
 end
