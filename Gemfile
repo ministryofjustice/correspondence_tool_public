e2bd4b974614bf17f60ba6d0f465c90867c0ccbb
Edward Andress <eddie_andress@hotmail.com>
2016-07-08 15:59:44 +0100

    add config gem and use settings.yml

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'slim'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'config'
 
 group :test do
   gem 'capybara'
--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -5,6 +5,8 @@ class Correspondence
 
   validates_presence_of :name, :email, :type, :message
   validates_confirmation_of :email
+  validates_inclusion_of :type, in: Settings.correspondence_types
+  validates_inclusion_of :sub_type, in: Settings.correspondence_sub_types, allow_nil: true
 
   attr_accessor :name, :email, :email_confirmation, :type, :sub_type, :message
 
