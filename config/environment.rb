51c50c59d9eaf45a393b2e5be0676a9ada79586d
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-07-15 18:03:23 +0100

    Switch from gmail to SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,10 @@ require_relative 'application'
 
 
 Rails.configuration.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "smtp.sendgrid.net",
     :port                 => 587,
-    :user_name            => ENV['CORRESPONDENCE_EMAIL_FROM'],
+    :domain               => 'digital.justice.gov.uk',
+    :user_name            => ENV['CORRESPONDENCE_EMAIL_USERNAME'],
     :password             => ENV['CORRESPONDENCE_EMAIL_PASSWORD'],
     :authentication       => :plain,
     :enable_starttls_auto => true
