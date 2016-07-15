ee76280f31711e8a9d565887f053001adc708f1f
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-07-15 07:48:45 +0100

    Added mail preview

--- spec/mailers/previews/correspondence_mailer_preview.rb
+++ spec/mailers/previews/correspondence_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/correspondence_mailer
 class CorrespondenceMailerPreview < ActionMailer::Preview
-
+  def new_correspondence_preview
+    @preview_correspondence = FactoryGirl.build(:correspondence)
+    CorrespondenceMailer.new_correspondence(@preview_correspondence)
+  end
 end
