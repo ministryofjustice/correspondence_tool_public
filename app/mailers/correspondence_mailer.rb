c3c062d80deb44ea5e1838dec35d172bce31685c
Edward Andress <eddie_andress@hotmail.com>
2016-07-15 14:00:20 +0100

    env var for FROM email address

--- app/mailers/correspondence_mailer.rb
+++ app/mailers/correspondence_mailer.rb
@@ -2,7 +2,7 @@ class CorrespondenceMailer < ApplicationMailer
 
   def new_correspondence(correspondence)
     @correspondence = correspondence
-    mail to: 'edward.andress@digital.justice.gov.uk', subject: "New #{@correspondence.type} - #{@correspondence.sub_type}"
+    mail to: ENV['CORRESPONDENCE_EMAIL_TO'], subject: "New #{@correspondence.type} - #{@correspondence.sub_type}"
   end
 
 end
