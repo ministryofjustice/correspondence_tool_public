958b0071423989d0ef2d4170e5f18bbe0a7eb417
Edward Andress <eddie_andress@hotmail.com>
2016-07-21 10:28:01 +0100

    grab appropriate TO address from env

--- app/mailers/correspondence_mailer.rb
+++ app/mailers/correspondence_mailer.rb
@@ -2,7 +2,7 @@ class CorrespondenceMailer < ApplicationMailer
 
   def new_correspondence(correspondence)
     @correspondence = correspondence
-    mail to: ENV['CORRESPONDENCE_EMAIL_TO'], subject: "New #{@correspondence.type} - #{@correspondence.topic}"
+    mail to: ENV["#{@correspondence.type.upcase}_EMAIL"], subject: "New #{@correspondence.type} - #{@correspondence.topic}"
   end
 
 end
--- spec/mailers/correspondence_mailer_spec.rb
+++ spec/mailers/correspondence_mailer_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe CorrespondenceMailer, type: :mailer do
     ActionMailer::Base.deliveries = []
     @correspondence = build(:correspondence)
     CorrespondenceMailer.new_correspondence(@correspondence).deliver_now
+    @mail = ActionMailer::Base.deliveries.first
   end
 
   after(:each) do
@@ -18,24 +19,37 @@ RSpec.describe CorrespondenceMailer, type: :mailer do
       expect(ActionMailer::Base.deliveries.count).to eq 1
     end
 
+    context 'to the correct address for' do
+      it 'freedom of information requests' do
+        expect(@mail.to).to eq [ ENV['FREEDOM_OF_INFORMATION_REQUEST_EMAIL'] ]
+      end
+
+      it 'treat official' do
+        ActionMailer::Base.deliveries.clear
+        @correspondence = build(:correspondence, type: 'treat_official')
+        CorrespondenceMailer.new_correspondence(@correspondence).deliver_now
+        @mail = ActionMailer::Base.deliveries.first
+        expect(@mail.to).to eq [ ENV['TREAT_OFFICIAL_EMAIL'] ]
+      end
+
+    end
+
     context 'and the subject contains' do
       it 'the type of correspondence' do
-        mail = ActionMailer::Base.deliveries.first
-        expect(mail.subject).to include(@correspondence.type)
+        expect(@mail.subject).to include(@correspondence.type)
       end
 
       it 'the area of interest' do
-        mail = ActionMailer::Base.deliveries.first
-        expect(mail.subject).to include(@correspondence.topic)
+        expect(@mail.subject).to include(@correspondence.topic)
       end
     end
 
     context 'and the body contains' do
       it 'a message from a member of the public' do
-        mail = ActionMailer::Base.deliveries.first
-        mail.html_part.body.to_s.include?(@correspondence.message)
+        expect(@mail.html_part.body.to_s).to include(@correspondence.message)
       end
     end
+
   end
 
 end
