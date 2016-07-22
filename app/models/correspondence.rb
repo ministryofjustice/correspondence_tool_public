d5c5df0c2c3ae96874de38dc7da4bca9ce5a69ae
Edward Andress <eddie_andress@hotmail.com>
2016-07-22 11:18:00 +0100

    add blank option to drop downs

--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -5,8 +5,8 @@ class Correspondence
 
   validates_presence_of :name, :email, :type, :message, :topic, :email_confirmation
   validates_confirmation_of :email, :case_sensitive => false
-  validates_inclusion_of :type, in: Settings.correspondence_types
-  validates_inclusion_of :topic, in: Settings.correspondence_topics
+  validates_inclusion_of :type, in: Settings.correspondence_types, if: Proc.new { |correspondence| correspondence.type.present? }
+  validates_inclusion_of :topic, in: Settings.correspondence_topics, if: Proc.new { |correspondence| correspondence.topic.present? }
 
   attr_accessor :name, :email, :email_confirmation, :type, :topic, :message
 
