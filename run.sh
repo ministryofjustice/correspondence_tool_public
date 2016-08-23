5df1aad61c3e1b9a15715ad7d9be10229e30a3e5
Jonathan Conway <jonathan@jaikoo.com>
2016-08-23 14:17:18 +0100

    add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export RAILS_ENV=production
+cd /usr/src/app
+case ${DOCKER_STATE} in
+create)
+    echo "running create"
+    bundle exec rake db:setup
+    ;;
+migrate)
+    echo "running migrate and seed"
+    bundle exec rake db:migrate
+    ;;
+esac
+bundle exec puma -d -C config/puma.rb
