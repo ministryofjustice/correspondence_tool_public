1f906f729e9c29656b9044806ed53719ba38cd33
Edward Andress <eddie_andress@hotmail.com>
2016-09-30 15:19:48 +0100

    Order of validations == order of errors on page

--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -3,18 +3,24 @@ class Correspondence
   include ActiveModel::Model
   include ActiveModel::Validations
 
-  validates_presence_of :name, :email, :email_confirmation, :type, :message, :topic
+  validates_presence_of :topic,
+                        :message,
+                        :name,
+                        :email,
+                        :email_confirmation,
+                        :type
 
   validates :email, confirmation: { case_sensitive: false }
 
-  validates_format_of :email, with: /@/
+  validates_format_of :email, with: /@/,
+    if: Proc.new { email.present? }
 
   validates_inclusion_of :type,
     in: Settings.correspondence_types,
-    if: Proc.new { |correspondence| correspondence.type.present? }
+    if: Proc.new { type.present? }
   validates_inclusion_of :topic,
     in: Settings.correspondence_topics,
-    if: Proc.new { |correspondence| correspondence.topic.present? }
+    if: Proc.new { topic.present? }
 
   attr_accessor :name, :email, :type, :topic, :message
 
