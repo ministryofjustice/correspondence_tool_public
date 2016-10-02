a0e13369b90f3b701413c5082035281c67747552
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-10-02 16:34:21 +0100

    Setup the Feedback Mailer

---
+++ spec/mailers/feedback_mailer_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe FeedbackMailer, type: :mailer do
+
+  def send_email
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+    @feedback = build(:feedback)
+    FeedbackMailer.new_feedback(@feedback).deliver_now
+    @mail = ActionMailer::Base.deliveries.first
+
+  end
+
+  before(:each) do
+    send_email
+  end
+
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  describe '#new_feedback' do
+
+    it 'should send an email' do
+      expect(ActionMailer::Base.deliveries.count).to eq 1
+    end
+
+    it 'should send to the correct address' do
+      expect(@mail.to).to eq [ ENV['AAQ_FEEDBACK_EMAIL'] ]
+    end
+
+    it 'should have a subject that contains the rating' do
+      expect(@mail.subject).to include(@feedback.rating.humanize)
+    end
+
+  end
+
+end
