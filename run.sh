264e286ab2a5219c38d7e48946b350bbaf9c70a2
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-11-03 10:30:46 +0000

    CT-196 Add three build environment variables
    As part of PR 75 we need three new vars to pick up for ping.json
    
    https://github.com/ministryofjustice/correspondence_tool_public/pull/75

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export RAILS_ENV=production
+export SETTINGS__GIT_COMMIT = $APP_GIT_COMMIT
+export SETTINGS__BUILD_DATE = $APP_BUILD_DATE
+export SETTINGS__GIT_SOURCE = $APP_BUILD_TAG
 cd /usr/src/app
 case ${DOCKER_STATE} in
 create)
