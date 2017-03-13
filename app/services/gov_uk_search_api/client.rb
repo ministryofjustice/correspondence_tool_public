827e24f4fa57cdb6742c7c3519ab2d6964e73fc6
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2017-03-13 11:09:55 +0000

    CT-508 Improve GOVUK Search API call
    This implements a basic snowball stop list of words to
    filter to. It takes the original topic and removes the stop words.
    
    We are using a gem called stoplist-filter which has its own list of
    stops words that can be extended. It also has locales support should
    the service need to be translated into other languages.

--- app/services/gov_uk_search_api/client.rb
+++ app/services/gov_uk_search_api/client.rb
@@ -6,29 +6,32 @@ module GovUkSearchApi
    NUM_RESULTS = 3
 
     def initialize(query)
-      @raw_query = query
-      @query = URI.encode(query)
+      @original_query = query
+
+      filter = Stopwords::Snowball::Filter.new "en"
+      filtered_words = filter.filter(@original_query.split)
+      @filtered_query = URI.encode(filtered_words.join(' '))
     end
 
     def search
-      CtpCustomLogger.info({event: 'query_search', search_term: @raw_query})
+      CtpCustomLogger.info({event: 'query_search', search_term: @original_query})
       begin
         curl_response = Curl.get(json_query_url)
-        Response.new(@raw_query, curl_response)
+        Response.new(@original_query, curl_response)
       rescue => err
-        Rails.logger.error(event: 'query_search', search_term: @raw_query, error_class: err.class, message: err.message)
-        Response.new(@raw_query, {error: {event: 'query_search', search_term: @raw_query, error_class: err.class.to_s, message: err.message}}.to_json)
+        Rails.logger.error(event: 'query_search', search_term: @original_query, error_class: err.class, message: err.message)
+        Response.new(@original_query, {error: {event: 'query_search', search_term: @original_query, error_class: err.class.to_s, message: err.message}}.to_json)
       end
     end
 
-   def html_query_url
-     "#{Settings.gov_uk_search_url}?q=#{@query}"
+   def more_results_url
+     "#{Settings.gov_uk_search_url}?q=#{@filtered_query}"
    end
 
     private
 
     def json_query_url
-      "#{Settings.gov_uk_search_api_url}?count=#{NUM_RESULTS}&q=#{@query}"
+      "#{Settings.gov_uk_search_api_url}?count=#{NUM_RESULTS}&q=#{@filtered_query}"
     end
   end
 end
