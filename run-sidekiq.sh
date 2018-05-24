0e0aa65718b12629ca174ce32847d1e847586bc6
Alistair Laing <aliuk2012@users.noreply.github.com>
2018-05-24 20:56:29 +0100

    dockerises the build for local development

--- run-sidekiq.sh
+++ run-sidekiq.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-bundle exec rackup sidekiq-admin.ru -p 3000  -o 0.0.0.0 &
-bundle exec sidekiq --daemon -l /var/log/sidekiq.log
-
-tail -f /usr/src/app/log/production.log
+bundle exec sidekiq -C config/sidekiq-background-jobs.yml
