634de8b9327b969a62b943f7e6abc31381c043a4
vampireinsun <yikang.mao@yahoo.com>
2020-05-21 11:45:26 +0100

    fixed typo on puma file

--- config/puma.rb
+++ config/puma.rb
@@ -42,7 +42,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 #
 on_worker_boot do
     ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-  end
+end
   
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
