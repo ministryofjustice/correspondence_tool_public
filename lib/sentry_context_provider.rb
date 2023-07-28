2b1869d1a8fff4a4e8316f6d97bc58b2eec68c04
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-07-28 14:26:19 +0100

    remove user logging

--- lib/sentry_context_provider.rb
+++ lib/sentry_context_provider.rb
@@ -1,10 +1,5 @@
 class SentryContextProvider
   def self.set_context(controller = nil)
-    if controller.respond_to?(:current_user) && controller.current_user.present?
-      user = controller.current_user
-      Sentry.set_user(id: user.id, email: user.email)
-    end
-
     Sentry.set_extras(
       host_environment: ENV["ENV"] || "Not set",
       build_date: ENV["APP_BUILD_DATE"],
