b593cd7ace9461699f44d0e68257657265e69bc5
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-07-28 10:37:52 +0100

    correct rubocop detected errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
 Sentry.init do |config|
-    config.dsn = 'https://ea2d904892c54e25954261508b9c1ccf@o345774.ingest.sentry.io/5407958'
-    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.dsn = "https://ea2d904892c54e25954261508b9c1ccf@o345774.ingest.sentry.io/5407958"
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 end
