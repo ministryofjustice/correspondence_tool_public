19cfd623df7bbdb5b1df965a18956902b5dccd54
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-07-28 12:24:35 +0100

    add sentry context provider

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,8 @@
-Sentry.init do |config|
-  config.dsn = "https://ea2d904892c54e25954261508b9c1ccf@o345774.ingest.sentry.io/5407958"
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+if ENV["SENTRY_DSN"].present?
+  return if %w[test development].include?(Rails.env)
+
+  Sentry.init do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.breadcrumbs_logger = [:active_support_logger]
+  end
 end
--- lib/sentry_context_provider.rb
+++ lib/sentry_context_provider.rb
@@ -1,14 +1,14 @@
 class SentryContextProvider
-    def self.set_context(controller = nil)
-      if controller.respond_to?(:current_user) && controller.current_user.present?
-        user = controller.current_user
-        Sentry.set_user(id: user.id, email: user.email)
-      end
-
-      Sentry.set_extras(
-        host_environment: ENV["ENV"] || "Not set",
-        build_date: ENV["APP_BUILD_DATE"],
-        git_commit_sha: ENV["APP_GIT_COMMIT"],
-      )
+  def self.set_context(controller = nil)
+    if controller.respond_to?(:current_user) && controller.current_user.present?
+      user = controller.current_user
+      Sentry.set_user(id: user.id, email: user.email)
     end
+
+    Sentry.set_extras(
+      host_environment: ENV["ENV"] || "Not set",
+      build_date: ENV["APP_BUILD_DATE"],
+      git_commit_sha: ENV["APP_GIT_COMMIT"],
+    )
   end
+end
