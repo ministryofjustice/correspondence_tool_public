a1f2bd39cd27ba50494166f6b6b7f1dfc75d4896
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-08-01 13:45:26 +0100

    add guard clause to restrict capturing error in dev & test environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Sentry.init do |config|
+  config/initializers/sentry.rb
   config.dsn = "https://ea2d904892c54e25954261508b9c1ccf@o345774.ingest.sentry.io/5407958"
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 end
