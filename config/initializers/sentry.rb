70cb0ab04e4b856c1fc81ae38dbb9366a6f5f245
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-08-01 13:52:15 +0100

    correct syntax error

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,8 @@
 if ENV["SENTRY_DSN"].present?
   return if %w[test development].include?(Rails.env)
 
-Sentry.init do |config|
-  config.dsn = "https://ea2d904892c54e25954261508b9c1ccf@o345774.ingest.sentry.io/5407958"
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  Sentry.init do |config|
+    config.dsn = "https://ea2d904892c54e25954261508b9c1ccf@o345774.ingest.sentry.io/5407958"
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  end
 end
