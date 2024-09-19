fa2a64db034c8f1938e50ea89c105250cfabeacb
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-09-19 11:13:36 +0100

    Use HTTParty instead of Curl

--- spec/services/gov_uk_search_api/client_spec.rb
+++ spec/services/gov_uk_search_api/client_spec.rb
@@ -9,11 +9,11 @@ module GovUkSearchApi
       let(:url_query) { "https://www.gov.uk/api/search.json?count=3&q=#{ERB::Util.url_encode(filtered_query)}" }
 
       context "when successful call to API" do
-        it "calls Curl with the URI encoded query and the curl response and returns a Response object" do
-          curl_easy = instance_double Curl::Easy
+        it "calls httparty with the URI encoded query and the httparty response and returns a Response object" do
+          httparty = instance_double HTTParty
           response = instance_double Response
-          allow(Curl).to receive(:get).with(url_query).and_return(curl_easy)
-          allow(Response).to receive(:new).with(query, curl_easy).and_return(response)
+          allow(HTTParty).to receive(:get).with(url_query).and_return(httparty)
+          allow(Response).to receive(:new).with(query, httparty).and_return(response)
 
           actual_response = described_class.new(query).search
           expect(actual_response).to eq response
@@ -22,7 +22,7 @@ module GovUkSearchApi
 
       context "when unsuccessful_call to API" do
         it "instantiates an empty result with error details" do
-          expect(Curl).to receive(:get).with(url_query).and_raise(Curl::Err::GotNothingError, "No data")
+          expect(HTTParty).to receive(:get).with(url_query).and_raise(HTTParty::ResponseError, "No data")
           response = described_class.new(query).search
           expect(response).to be_instance_of(Response)
           expect(response.num_items).to eq 0
@@ -30,7 +30,7 @@ module GovUkSearchApi
             "error" => {
               "event" => "query_search",
               "search_term" => "Ministry of Justice",
-              "error_class" => "Curl::Err::GotNothingError",
+              "error_class" => "HTTParty::ResponseError",
               "message" => "No data",
             },
           })
