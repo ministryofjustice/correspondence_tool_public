7612b3415ca9d2b031b7508c1bf204d723897f85
Tony Mckernan <anthony.mckernan@justice.gov.uk>
2026-07-29 10:47:56 +0100

    CDPTKAN-1221  Switch on Content Security Policy (#914)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,25 +1,16 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self
+    policy.font_src    :self
+    policy.img_src     :self, :data
+    policy.object_src  :none
+    policy.script_src  :self, "https://www.googletagmanager.com"
+    policy.style_src   :self
+    policy.frame_src   "https://www.googletagmanager.com"
+    policy.connect_src :self
+    policy.base_uri    :self
+  end
 
-# Define an application-wide content security policy.
-# See the Securing Rails Applications Guide for more information:
-# https://guides.rubyonrails.org/security.html#content-security-policy-header
-
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+  config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
+  config.content_security_policy_nonce_directives = %w[script-src]
+end
